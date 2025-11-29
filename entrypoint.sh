7045c6321d966eb135f1473a06b23359455d785e
JuanC1824 <juanjosecardenas142006@gmail.com>
2025-11-29 11:55:14 -0500

    Cambios

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-# Reemplaza __API_URL__ con la variable de entorno BACKEND_URL de Render
-sed -i "s|__API_URL__|${BACKEND_URL}|g" /usr/share/nginx/html/assets/environment.js
+sed -i "s|__API_URL__|${BACKEND_URL}|g" /usr/share/nginx/html/browser/assets/environment.js
 nginx -g 'daemon off;'
